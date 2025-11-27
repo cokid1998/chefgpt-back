31408bae6b6d386679be8883620c6354b181ff84
이태관 <cokid98@naver.com>
2025-11-27 10:44:16 +0900

    Feat: app.module에 prismaModule을 import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
 import { ConfigModule } from "@nestjs/config";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule, PrismaModule],
   controllers: [],
   providers: [],
 })
