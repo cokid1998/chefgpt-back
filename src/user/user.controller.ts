d702c9af4c79793409c1c1c18e85fb6b8653d22f
이태관 <cokid98@naver.com>
2025-12-02 09:43:02 +0900

    Feat: 유저정보 API 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
+import { ApiOperation } from "@nestjs/swagger";
+import { UserService } from "src/user/user.service";
 
 @Controller("user")
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get(":id")
+  @ApiOperation({
+    summary: "유저정보",
+  })
+  findUserById(@Param("id") id: string) {
+    return this.userService.findUserById(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { BadRequestException } from "@nestjs/common";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findUserById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!user) {
+      throw new BadRequestException("해당 유저를 찾을 수 없습니다.");
+    }
+
+    return user;
+  }
+}
