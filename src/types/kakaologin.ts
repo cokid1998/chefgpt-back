d45bd43500bc37ce4e60e61d4c81651e994db8b3
이태관 <cokid98@naver.com>
2025-12-29 20:28:20 +0900

    chore: 카카오에 요청하는 응답데이터 타입 정의

---
+++ src/types/kakaologin.ts
@@ -0,0 +1,30 @@
+export interface KAKAOOauthRes {
+  access_token: string;
+  token_type: "bearer";
+  refresh_token: string;
+  id_token: string;
+  expires_in: number;
+  scope: string;
+  refresh_token_expires_in: number;
+}
+
+export interface KAKAOUserRes {
+  id: number;
+  connected_at: string; // ex)"2025-12-29T09:42:40Z";
+  properties: {
+    nickname: string;
+    profile_image: string;
+    thumbnail_image: string;
+  };
+  kakao_account: {
+    profile_nickname_needs_agreement: boolean;
+    profile_image_needs_agreement: boolean;
+    profile: {
+      nickname: string;
+      thumbnail_image_url: string;
+      profile_image_url: string;
+      is_default_image: boolean;
+      is_default_nickname: boolean;
+    };
+  };
+}
