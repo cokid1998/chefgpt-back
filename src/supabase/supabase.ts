8cfa9b6c0cbe92e1d9fc4bb837403d5d71b3a390
이태관 <cokid98@naver.com>
2026-02-04 15:03:19 +0900

    Fix: supabase파일에서 환경변수를 가져오지 못하는 문제 해결
    - supabase 관련 환경변수 전부 수정

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,11 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
+import * as dotenv from "dotenv";
 
-// Todo: env파일 못불러오는 문제
+dotenv.config();
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 export const BUCKET_NAME = "uploads";
-export const supabase = createClient(
-  "https://tqgpealdunegqnxqemvz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxZ3BlYWxkdW5lZ3FueHFlbXZ6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODgyMTAyOSwiZXhwIjoyMDg0Mzk3MDI5fQ.iFVq4x-gk5mjQIUx8x76C4wQQ3p6nj9ea3DJ-9bsmIo",
-);
+export const supabase = createClient(supabaseUrl, supabaseKey);
