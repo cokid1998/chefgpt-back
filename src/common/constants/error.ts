e1c4316a3665cc63f4ffdae288fd4adb621f3763
이태관 <cokid98@naver.com>
2026-05-06 15:22:48 +0900

    feat: 에러 상수 파일 추가

---
+++ src/common/constants/error.ts
@@ -0,0 +1,11 @@
+export const ERROR = {
+  // 유튜브 관련 에러 객체
+  NO_CAPTION: {
+    code: "NO_CAPTION",
+    message: "자막이 없는 영상입니다.",
+  },
+  NOT_RECIPE: {
+    code: "NOT_RECIPE",
+    message: "요리와 관련없는 영상입니다.",
+  },
+} as const;
